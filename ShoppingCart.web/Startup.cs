b894eb5a54d00b9b879949334b858c4aca51a8f7
pymkumar19313@gmail.com <pymkumar19313@gmail.com>
2019-07-15 19:18:20 +0530

    project startup

---
+++ ShoppingCart.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingCart.web.Startup))]
+namespace ShoppingCart.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
